d0a44f6236801117032131afc07b4489bc8df18f
Hanma-yujiro <mpethamotaung@gmail.com>
2021-04-18 15:10:10 +0200

    user com

--- Bank Account/User.java
+++ Bank Account/User.java
@@ -1,3 +1,18 @@
+/*
+
+*/
+
+//java packages import
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class User {
+
+    private String name;
+    private ArrayList<Account> accounts;
+
+    public User(String name) {
+
+    }
 
 }
