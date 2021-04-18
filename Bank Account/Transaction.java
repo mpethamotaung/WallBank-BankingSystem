247b21be19e1791b0a0e877aa0e1324615bdea75
Hanma-yujiro <mpethamotaung@gmail.com>
2021-04-18 15:43:28 +0200

    transaction commit

--- Bank Account/Transaction.java
+++ Bank Account/Transaction.java
@@ -4,4 +4,46 @@
 
 public class Transaction {
 
+    // initializing instance variables
+    private double amount;
+    private Date timestamp;
+    private String memo;
+    private Account inAccount;
+
+    //
+    public Transaction(double amount, Account inAccount) {
+
+        // set transaction args
+        this.amount = amount;
+        this.inAccount = inAccount;
+
+        // set timestamp and memo
+        this.timestamp = new Date();
+        this.memo = "";
+    }
+
+    //
+    public Transaction(double amount, String memo, Account inAccount) {
+
+        // call the two args
+        this(amount, inAccount);
+
+        // set the memo
+        this.memo = memo;
+    }
+
+    // get method that returns amount
+    public double getAmount() {
+        return this.amount;
+    }
+
+    // get method returns summary
+    public String getSummaryLine(){
+        if (this.amount >= 0){
+            return String.format("%s : $%.02f :%s,",this.timestamp.toString(),this.amount,this.memo);
+        }else{
+        return String.format("%s : $(%.02f) :%s,",this.timestamp.toString(),this.amount,this.memo);
+        
+    }
+
 }
