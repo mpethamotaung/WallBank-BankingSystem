a8b18166f50e8fb19924f7cc6c463080a65f33ab
Hanma-yujiro <mpethamotaung@gmail.com>
2021-04-19 18:38:21 +0200

    deposit into savings account functions

--- Bank Account(sub)/Bank.java
+++ Bank Account(sub)/Bank.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Bank Sys operates with Account objects
 */
@@ -8,16 +10,27 @@ public class Bank {
     public static void main(String[] args) {
 
         // Create Savings account object
-        SavingsAccount account = new SavingsAccount(1000);
+        SavingsAccount account1 = new SavingsAccount(1000);
 
         //Display InitialBalance of Each Account
-        System.out.printf("Savings Account Balance: $%.2f \n", account.getBalance());
+        System.out.printf("Savings Account Balance: $%.2f \n", account1.getBalance());
 
         //Create Scanner to obtain input
         Scanner input = new Scanner(System.in);
 
         //initialize instance variable to store deposit amount
         double depositAmount;
+
+        /* ------------- Deposit ---------------- */
+        //prompt user to enter deposit amount
+        System.out.print("Enter deposit amount");
         
+        //set deposit amount = user input(int)
+        depositAmount = input.nextDouble();
+
+        System.out.printf(" \n adding %.2f to Savings Account\n\n", depositAmount);
+        account1.deposit(depositAmount);
+
+        /* --------------- Withdraw -------------- */
 
 }
