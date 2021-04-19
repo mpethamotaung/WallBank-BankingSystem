79199588cd5eb5b0a3a5a300e1ebab21d5238853
Hanma-yujiro <mpethamotaung@gmail.com>
2021-04-19 18:07:19 +0200

    create account class and constructor

--- Bank Account(sub)/Account.java
+++ Bank Account(sub)/Account.java
@@ -1,7 +1,19 @@
 /*
-
+Account class
 */
 
+//create account class
 public class Account {
 
+    // initialize instance variables to store values
+    private double balance;
+
+    // account class constructor
+    public Account(double initialBalance) {
+
+        if (initialBalance > 0.0) {
+
+            balance = initialBalance;
+        }
+    }
 }
