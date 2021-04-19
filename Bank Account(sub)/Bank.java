6580c27bc3ca72999ae321050e727e2cb29dc1fa
Hanma-yujiro <mpethamotaung@gmail.com>
2021-04-19 18:32:28 +0200

    savings account object, print initial balance, add scanner input, depositamount variable

--- Bank Account(sub)/Bank.java
+++ Bank Account(sub)/Bank.java
@@ -9,5 +9,15 @@ public class Bank {
 
         // Create Savings account object
         SavingsAccount account = new SavingsAccount(1000);
-    }
+
+        //Display InitialBalance of Each Account
+        System.out.printf("Savings Account Balance: $%.2f \n", account.getBalance());
+
+        //Create Scanner to obtain input
+        Scanner input = new Scanner(System.in);
+
+        //initialize instance variable to store deposit amount
+        double depositAmount;
+        
+
 }
